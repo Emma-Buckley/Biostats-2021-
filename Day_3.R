4019cb211c8a5d3903db57f5451635e9d954e1da
Emma <3832644@myuwc.ac.za>
2021-04-24 18:13:11 +0200

    Day 3 Commit

---
+++ Day_3.R
@@ -0,0 +1,115 @@
+#Emma Buckley
+#21 April 2021
+#Linear Regressions
+#Day 3
+
+#Loading tidyverse
+
+library(tidyverse)
+
+#Getting data faithful
+
+data("faithful")
+
+#First six rows of faithful
+
+head(faithful)
+
+#Creating a sub dataframe called eruption.lm.
+#fit the model in R. When we perform a linear regression in R, it will 
+#output the model and the coefficients
+
+eruption.lm <- lm(eruptions ~ waiting, data = faithful)
+
+#summarising the dataframe
+summary(eruption.lm)
+
+slope <- round(eruption.lm$coef[2], 3)
+
+# p.val <- round(coefficients(summary(eruption.lm))[2, 4], 3) # it approx. 0.
+p.val = 0.001
+
+r2 <- round(summary(eruption.lm)$r.squared, 3)
+
+#Creating a ggplot
+
+ggplot(data = faithful, aes(x = waiting, y = eruptions)) + #creating a plot with points
+  geom_point() +#point plot
+  annotate("text", x = 45, y = 5, label = paste0("slope == ", slope, "~(min/min)"), parse = TRUE, hjust = 0) +
+  annotate("text", x = 45, y = 4.75, label = paste0("italic(p) < ", p.val), parse = TRUE, hjust = 0) +
+  annotate("text", x = 45, y = 4.5, label = paste0("italic(r)^2 == ", r2), parse = TRUE, hjust = 0) +#adding text to the graph
+  stat_smooth(method = "lm", colour = "red") +
+  labs(title = "Old Faithful eruption data", #adding labels
+       subtitle = "Linear regression",
+       x = "Waiting time (minutes)",
+       y = "Eruption duration (minutes)")
+
+#Part 2
+#Emma Buckley
+#21 April 2021
+#Correlations
+#Day 3
+
+install.packages("ggpubr")
+install.packages("corrplot")
+
+# Load libraries #activate the packages
+
+library(tidyverse)
+library(ggpubr)
+library(corrplot)
+
+# Load data
+
+ecklonia <- read_csv("~/Biostatistics/Second part of R/Biostats-2021/data/ecklonia.csv")
+
+#Removing categorical variables 
+#create a subsetted version of our data by removing all of the categorical variables
+
+ecklonia_sub <- ecklonia %>% 
+  select(-species, - site, - ID)
+
+# Perform correlation analysis on two specific variables
+# Note that we do not need the final two arguments in this function to be stated
+# as they are the default settings.
+# They are only shown here to illustrate that they exist.
+#comparing two variables
+
+cor.test(x = ecklonia$stipe_length, ecklonia$frond_length, #specifying a column
+         use = "everything", method = "pearson")
+
+#Now we want to compare many variables
+ecklonia_pearson <- cor(ecklonia_sub)
+ecklonia_pearson
+
+#Kendall rank correlation
+ecklonia_norm <- ecklonia_sub %>% 
+  gather(key = "variable") %>% 
+  group_by(variable) %>% 
+  summarise(variable_norm = as.numeric(shapiro.test(value)[2]))
+ecklonia_norm
+
+cor.test(ecklonia$primary_blade_length, ecklonia$primary_blade_width, method = "kendall")
+
+
+#One panel visual
+
+# Calculate Pearson r beforehand for plotting
+#creating label of the r value
+r_print <- paste0("r = ", 
+                  round(cor(x = ecklonia$stipe_length, ecklonia$frond_length),2))
+
+# Then create a single panel showing one correlation
+ggplot(data = ecklonia, aes(x = stipe_length, y = frond_length)) +
+  geom_smooth(method = "lm", colour = "grey90", se = F) +
+  geom_point(colour = "mediumorchid4") +
+  geom_label(x = 300, y = 240, label = r_print) +
+  labs(x = "Stipe length (cm)", y = "Frond length (cm)") +
+  theme_pubclean()
+
+#creates a correlation plot
+
+corrplot(ecklonia_pearson, method = "circle")
+
+#If the colour is dark = strong correlation
+#if the colour is lighter = weak correlation
